0ca84296a2248d73ebead87a59ca7ffd4ea61626
William Lay <laywill@gmail.com>
2025-12-23 01:22:39 +0000

    Fix: esling.config.js lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,34 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   {
-    ignores: ["dist/", ".vite/", "node_modules/", "megalinter-reports/"],
+    ignores: ['dist/', '.vite/', 'node_modules/', 'megalinter-reports/']
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     languageOptions: { globals: globals.browser },
     plugins: {
-      "react-hooks": pluginReactHooks,
+      'react-hooks': pluginReactHooks
     },
     settings: {
       react: {
-        version: "detect",
-      },
-    },
+        version: 'detect'
+      }
+    }
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-    },
-  },
-]);
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  }
+])
